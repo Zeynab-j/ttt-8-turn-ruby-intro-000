27b2358da54cd7609b4a2d8a73729ddd8cb1eaf4
Zeynab Jibreel <zeina2025@gmail.com>
2020-02-22 22:32:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,11 +13,7 @@ end
 def turn(board)
   puts "Please enter 1-9:"
 end
-def input_to_index(user_input)
-  index = user_input.to_i
-  index -= 1
-  return index
-end
+
 def move(board, index, character = "X")
   board[index] = character
   return board
