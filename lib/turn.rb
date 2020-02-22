af65428bea7fd12f7adddbf19ca59c84940d00bb
Zeynab Jibreel <zeina2025@gmail.com>
2020-02-22 22:31:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -26,11 +26,4 @@ def valid_move?(board, index)
    if index.between?(0,8) && !position_taken?(board, index)
       return true
    end
-def turn (board)
-  puts "Please enter 1-9:"
-  num = gets.chomp
-  index = input_to_index(num)
-  if valid_move?(board, index) == true
-    move(board, index)
-    display_board(board)
-  else
+
